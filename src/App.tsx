a5a7c94b60f4418ba1176a2300d3b6e9ce349e4b
thiepn <junsosmart@gmail.com>
2026-08-31 20:08:48 +0200

    feat: mount PWA lifecycle status

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { AppShell } from './app/AppShell';
+import { PwaStatus } from './app/PwaStatus';
 import { ThemeProvider } from './theme/ThemeProvider';
 
 export function App() {
   return (
     <ThemeProvider>
       <AppShell />
+      <PwaStatus />
     </ThemeProvider>
   );
 }
