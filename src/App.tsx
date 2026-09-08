249abb01f56789918d24dc833ab9b8829bebf8e0
thiepn <junsosmart@gmail.com>
2026-09-08 21:09:06 +0200

    Wire cloud sync provider into app

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,20 @@ import { PwaStatus } from './app/PwaStatus';
 import { PrivacyGate } from './features/privacy/PrivacyGate';
 import { PrivacyProvider } from './features/privacy/PrivacyProvider';
 import { ReminderNotificationCoordinator } from './features/reminders/ReminderNotificationCoordinator';
+import { SyncProvider } from './features/sync/SyncProvider';
 import { ThemeProvider } from './theme/ThemeProvider';
 
 export function App() {
   return (
     <ThemeProvider>
       <PrivacyProvider>
-        <PrivacyGate>
-          <AppShell />
-          <PwaStatus />
-        </PrivacyGate>
-        <ReminderNotificationCoordinator />
+        <SyncProvider>
+          <PrivacyGate>
+            <AppShell />
+            <PwaStatus />
+          </PrivacyGate>
+          <ReminderNotificationCoordinator />
+        </SyncProvider>
       </PrivacyProvider>
     </ThemeProvider>
   );
