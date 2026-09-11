38d3e0917e874d4822384cefeed86529f8faefa5
thiepn <junsosmart@gmail.com>
2026-09-11 14:12:59 +0200

    P15: format related-note tests

--- src/features/links/relatedNotes.test.ts
+++ src/features/links/relatedNotes.test.ts
@@ -1,11 +1,7 @@
 import { describe, expect, it } from 'vitest';
 
 import type { LabelRecord, NoteRecord } from '../../db';
-import {
-  findRelatedNotes,
-  suggestLabelsForNote,
-  summarizeLocalTopics,
-} from './relatedNotes';
+import { findRelatedNotes, suggestLabelsForNote, summarizeLocalTopics } from './relatedNotes';
 
 function note(
   id: string,
